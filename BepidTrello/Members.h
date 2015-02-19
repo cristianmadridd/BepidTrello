37f7836647b2bc53ddcf2dfe2365fa6f24190747
Anderson  Kloss Maia <andersonklossmaia@MacBook-Pro-de-Anderson.local>
2015-02-19 15:54:25 -0200

    Class Members added

---
+++ BepidTrello/Members.h
@@ -0,0 +1,14 @@
+//
+//  Members.h
+//  BepidTrello
+//
+//  Created by Anderson  Kloss Maia on 19/02/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Members.h"
+
+@interface Members : NSObject
+
+@end
---
+++ BepidTrello/Members.m
@@ -0,0 +1,13 @@
+//
+//  Members.m
+//  BepidTrello
+//
+//  Created by Anderson  Kloss Maia on 19/02/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import "Members.h"
+
+@implementation Members
+
+@end
