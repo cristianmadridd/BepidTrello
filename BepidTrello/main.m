a2057799fb66783302bf0d9fad6d9a48c77ac4d4
cristianmadridd <cristianmadridd@gmail.com>
2015-02-19 13:20:35 -0200

    First commit

---
+++ BepidTrello/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BepidTrello
+//
+//  Created by Cristian Madrid on 2/19/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
