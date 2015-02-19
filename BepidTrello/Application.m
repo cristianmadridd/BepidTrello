4463a9bcdd43dedd54026cc83ee5190a50e6bc64
Anderson  Kloss Maia <andersonklossmaia@MacBook-Pro-de-Anderson.local>
2015-02-19 15:13:53 -0200

    Class Application added and main modified

---
+++ BepidTrello/Application.m
@@ -0,0 +1,24 @@
+//
+//  Application.m
+//  BepidTrello
+//
+//  Created by Anderson  Kloss Maia on 19/02/15.
+//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Application : NSObject
+
+-(void)initialize;
+
+@end
+
+@implementation Application
+
+-(void)initialize
+{
+    NSLog(@"Welcome to Trello");
+}
+
+@end
