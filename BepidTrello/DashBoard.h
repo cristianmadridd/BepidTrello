c04181a90f328cca283267abe8141707bf6960ef
elisaleoo <elisaleoo@gmail.com>
2015-02-19 15:18:43 -0200

    Editada a classe Lists.

--- BepidTrello/DashBoard.h
+++ BepidTrello/DashBoard.h
@@ -2,13 +2,16 @@
 //  DashBoard.h
 //  BepidTrello
 //
-//  Created by Cristian Madrid on 2/19/15.
-//  Copyright (c) 2015 Cristian Madrid. All rights reserved.
+//  Created by Alexandre Mantovani, Anderson Kloss, Cristian Madrid, Elisa Leo on 2/19/15.
+//  Copyright (c) 2015 Trello Group. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import "Organization.h"
 
 
-@interface DashBoard : NSObject
+@interface DashBoard : NSObject {
+    Organization *myOrganization;
+}
 
 @end
--- BepidTrello/Organization.m
+++ BepidTrello/Organization.m
@@ -16,7 +16,6 @@
     return nil;
 }
 
-+()
 
 
 @end
