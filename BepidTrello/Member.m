080c0eda5cfbfecbcb9e2de821b3573ec064a6b7
Cristian Madrid <cristianmadridd@gmail.com>
2015-02-20 07:35:16 -0200

    Add the methods getPassword, getUserName and getName

---
+++ BepidTrello/Member.m
@@ -0,0 +1,37 @@
+//
+//  Member.m
+//  BepidTrello
+//
+//  Created by Alexandre Mantovani, Anderson Kloss, Cristian Madrid, Elisa Leo on 2/19/15.
+//  Copyright (c) 2015 Trello Group. All rights reserved.
+//
+
+#import "Member.h"
+
+@implementation Member
+
+-(NSString *)getName{
+    return @"";
+}
+
+-(void)setName:(NSString *)name{
+
+}
+
+-(NSString *)getUserName{
+    return @"";
+}
+
+-(void)setUserName:(NSString *)userName{
+
+}
+
+-(NSString *)getPassword{
+    return @"";
+}
+
+-(void)setPassword:(NSString *)password{
+
+}
+
+@end
