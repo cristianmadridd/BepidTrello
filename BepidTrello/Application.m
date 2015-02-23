ccaff368f83cb83b9c8875660e054bc0a1332684
cristianmadridd <cristianmadridd@gmail.com>
2015-02-23 14:54:00 -0300

    Final version

--- BepidTrello/Application.m
+++ BepidTrello/Application.m
@@ -2,7 +2,7 @@
 //  Application.m
 //  BepidTrello
 //
-//  Created by Alexandre Mantovani, Anderson Kloss, Cristian Madrid, Elisa Leo on 2/19/15.
+//  Created by Alexandre Mantovani, Anderson Kloss, Cristian Madrid, Elisa Leo on 2/23/15.
 //  Copyright (c) 2015 Trello Group. All rights reserved.
 //
 
